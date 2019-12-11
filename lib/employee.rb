0d1003eb865078ca24c7dbf2c21ec4b95149925b
hildakh <h_khajehamiri@yahoo.com>
2019-12-11 19:29:33 +0000

    connected the employees table to the stores and created 4 employees:

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,11 @@
 class Employee < ActiveRecord::Base
-
+  belongs_to :store
 end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+@store1.employees.create(first_name: "Hilda", last_name: "Matilda", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Mary", last_name: "Poppins", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Melanie", last_name: "Parrot", hourly_rate: 60)
