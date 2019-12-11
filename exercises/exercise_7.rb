d74bb7912805c0f4e3b18ca5e0c7960d21025d88
hildakh <h_khajehamiri@yahoo.com>
2019-12-11 21:43:06 +0000

    created a new store with the name provided by the user and triggered the error by missing to include some validated data

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,13 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts 'Help us choose a store name'
+@newstore = gets.chomp
+# creating a store without providing the annual_revenue which is required(validated)
+store10 = Store.create(name: @newstore)
+
+puts store10.errors.full_messages
+
+store10 = Store.create(name: @newstore, annual_revenue: 20000)
+
+puts store10.errors.full_messages
