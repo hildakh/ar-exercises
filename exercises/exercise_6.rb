77a2ab4efa04993993ab57ad6ea78f55bcd6cfdf
hildakh <h_khajehamiri@yahoo.com>
2019-12-11 21:15:51 +0000

    completed ex 6 and added validation for stores and employees tables

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+@store1.employees.create(first_name: "Hilda", last_name: "Matilda", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Mary", last_name: "Poppins", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Melanie", last_name: "Parrot", hourly_rate: 60)
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,11 +1,9 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { greater_than: 40, less_than: 200}
+  validates :store, presence: true
+
 end
 
-@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
-
-@store1.employees.create(first_name: "Hilda", last_name: "Matilda", hourly_rate: 60)
-
-@store2.employees.create(first_name: "Mary", last_name: "Poppins", hourly_rate: 60)
-
-@store2.employees.create(first_name: "Melanie", last_name: "Parrot", hourly_rate: 60)
